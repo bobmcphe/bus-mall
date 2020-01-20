24e3c4ddb10d97ee4f37a7aac9655aeb81d234e4
bobby mcpherson <bobmcphe@gmail.com>
2020-01-20 14:24:15 -0600

    working code with 2 pic logic

--- JS/app.js
+++ JS/app.js
@@ -14,9 +14,10 @@ var centerIndex = null;
 
 // var picssArray = []
 
-var goatVotes = 0;
+var imageVote = 0;
 var totalRounds = 10;
 
+//My constructor function--------------------------------------------------------
 function AdImage(name, image){
   this.name = name;
   this.image = image;
@@ -33,48 +34,54 @@ function randomPic(){
   var randomNumber = Math.floor(Math.random() * AdImage.allImages.length);
   return randomNumber;
 }
+// closing comment - get a random number function-----------------------------
 
 
-
-//Create our picture clicking function. Must be defined before our event listener
+//Create-picture-clicking function. Must be defined before our event listener
 //when you pass a function as an argument, it's called a callback
 var handleClickOnImage = function(event){
-console.log(event.target.id);
-var imageClicked = event.target.id;
+    console.log(event.target.id, 'inside handle click');
+    var imageClicked = event.target.id;
 
-if(imageClicked === 'imageLeft' || imageClicked === 'imageRight'){
+    if(imageClicked === 'picLeft' || imageClicked === 'picRight'){
+        console.log('made into the first!!');
+        
+        imageVote++;
+        // need to incrament goat clicked by one
+        if(imageClicked === 'picLeft'){
+        // do logic to incrament the number
+        AdImage.allImages[leftIndex].clicked++;
+        console.log('you made it here left');
 
-    imageVote++;
-    // need to incrament goat clicked by one
-    if(imageClicked === 'imageLeft'){
-      // do logic to incrament the number
-      Goat.allImages[leftGoatIndex].clicked++;
-    } else if(imageClicked === 'imageRight'){
-      Goat.allImages[rightGoatIndex].clicked++;
+        } else if(imageClicked === 'picRight'){
+        AdImage.allImages[rightIndex].clicked++;
+        console.log('you made it here right');
+        }
+
+
+    } else{
+        alert('you didn\'t select an image');
+    } 
+    
+    console.log(AdImage.allImages[leftIndex]);
+    console.log(AdImage.allImages[rightIndex]);
+    
+    if(imageVote === 5){
+            // remove
+            adImagesTag.removeEventListener('click', handleClickOnImage);
+            console.log("you completed the voting")
+            // output to the browser the results
+            
+            for(var i=0; i < AdImage.allImages.length; i++) {
+            var X = AdImage.allImages[i];
+            console.log(`${AdImage.name} received ${AdImage.clicked} votes with ${AdImage.views} views`);
+            }
+                } else{
+                    renderAdImages();
+                }
+
     }
-  } else{
-    alert('you didn\'t select an image');
-  }
-
-   if(imageVote === 5){
-    // remove
-    adImagesTag.removeEventListener('click', handleClickOnGoat);
-    console.log("you completed the voting")
-    // output to the browser the results
-    // "Sassy Goat received 4 votes with 5 views"
-    for(var i=0; i < AdImage.allImages.length; i++)
-    {
-      var goat = AdImage.allImages[i];
-      console.log(`${goat.name} received ${AdImage.clicked} votes with ${AdImage.views} views`);
-    }
-  } else{
-    renderAdImages();
-  }
-
-}
-
-
-
+//closing comment - Create-picture-clicking function. Must be defined before our event listener
 
 function renderAdImages(){
   do{
@@ -89,7 +96,7 @@ function renderAdImages(){
 }
 
 
-pictureParent.addEventListener('click', handleClickOnImage)
+
 
 
 // Instantiations------------------------------------
@@ -98,14 +105,13 @@ AdImage.allImages = [];
 //we actually don't need the above instantiation, but can do as follows:
 
 new AdImage('fourth pic', "img/breakfast.jpg");
-
 new AdImage('fifth pic', "img/Bubblegum.jpg");
-
-new AdImage('sixth pic', "img/chair.jpg");
-
-// new AdImage('seventh pic', "https://images2.minutemediacdn.com/image/upload/c_crop,h_843,w_1500,x_0,y_10/f_auto,q_auto,w_1100/v1555172614/shape/mentalfloss/iStock-177369626_1.jpg");
+new AdImage('seventh pic', "img/chair.jpg");
+new AdImage('eighth pic', "img/cthulhu.jpg");
+new AdImage('ninth pic', "img/dog-duck.jpg");
+new AdImage('tenth pic', "img/dragon.jpg");
+new AdImage('eleventh pic', "img/pen.jpg");
 
 renderAdImages();
 
-
-
+pictureParent.addEventListener('click', handleClickOnImage);
