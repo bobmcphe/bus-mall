1ca96377b446898ea1b9f2134a4e8caaf9b25ee7
bobby mcpherson <bobmcphe@gmail.com>
2020-01-21 14:47:56 -0600

    partially working dual-data chart

--- JS/app.js
+++ JS/app.js
@@ -142,12 +142,12 @@ new AdImage('bag pic', "img/bag.jpg");
 function renderChart() {
 
     var labelData = [];
-    var chartData = [];
+    var clickData = [];
     var viewData = [];
 
   for (var i= 0; i< AdImage.allImages.length; i++){
     labelData.push(AdImage.allImages[i].name);
-    chartData.push(AdImage.allImages[i].clicked);
+    clickData.push(AdImage.allImages[i].clicked);
     viewData.push(AdImage.allImages[i].views);
     
       }
@@ -161,7 +161,7 @@ var myChart = new Chart(ctx, {
         labels: labelData,
         datasets: [{
             label: '# of Votes',
-            data: chartData,
+            data: clickData,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 // 'rgba(54, 162, 235, 0.2)',
@@ -179,29 +179,27 @@ var myChart = new Chart(ctx, {
                 // 'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
-        }],
-
-        datasets: [{
-            label: '# of Votes',
+        }, {
+            label: '# of Views',
             data: viewData,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
+                // 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                // 'rgba(255, 206, 86, 0.2)',
+                // 'rgba(75, 192, 192, 0.2)',
+                // 'rgba(153, 102, 255, 0.2)',
+                // 'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
+                // 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                // 'rgba(255, 206, 86, 1)',
+                // 'rgba(75, 192, 192, 1)',
+                // 'rgba(153, 102, 255, 1)',
+                // 'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
-        }]
+        }],
         
     },
     
